8b0dfce64657eee729cc3a1ab53dbdd0584d9ca8
expl-rel <laasyaraagyni@gmail.com>
2024-01-09 11:10:21 +0530

    Made changes to Benchmarks of MPSC queue

--- bench/bench_mpsc_queue.ml
+++ bench/bench_mpsc_queue.ml
@@ -29,16 +29,19 @@ let bench_single label create push pop =
   Printf.printf "%s  (one domain): %f per second\n%!" label
     (Float.of_int n_messages /. elapsed)
 
-let bench_single_length label create push pop =
+let bench_single_length length label create push pop =
   let elapsed =
     average @@ fun () ->
     let queue = create () in
     for i = 1 to n_messages do
-      if i > 100 && i mod 2 = 1 then pop queue else push queue i
+      if i > n_messages - length then pop queue
+      else if i > length && i mod 2 = 1 then pop queue
+      else push queue i
     done
   in
 
-  Printf.printf "%s  (one domain with queue length): %f per second\n%!" label
+  Printf.printf "%s  (one domain with queue length %d): %f per second\n%!" label
+    length
     (Float.of_int n_messages /. elapsed)
 
 let bench_single_seq list label create push pop =
@@ -63,6 +66,9 @@ let bench_single_seq list label create push pop =
 
 let rec generate_list length queue_len (list : bool list) =
   if length = 0 then list
+  else if (* length here is the length of the list required *)
+          length = queue_len
+  then generate_list (length - 1) (queue_len + 1) (false :: list)
   else
     let rand = Random.bool () in
     if rand = true then generate_list (length - 1) (queue_len + 1) (rand :: list)
@@ -79,7 +85,10 @@ let bench_single_rand label create push pop =
     let len = ref 0 in
     let queue = create () in
     for i = 1 to n_messages do
-      if !len > 1000 then (
+      if !len = n_messages - i + 1 then (
+        len := !len - 1;
+        pop queue)
+      else if !len > 1000 then (
         len := !len - 1;
         pop queue)
       else if !len = 0 then (
@@ -102,14 +111,23 @@ let bench_single_rand label create push pop =
 let bench_single_all label create push pop is_empty push_head peek =
   let elapsed =
     average @@ fun () ->
+    let len = ref 0 in
     let queue = create () in
     for i = 1 to n_messages do
-      let rand = Random.int 7 in
-      if rand < 2 then ignore @@ push queue i
-      else if rand < 4 then ignore @@ pop queue
-      else if rand < 5 then ignore @@ push_head queue i
-      else if rand < 6 then ignore @@ is_empty queue
-      else if rand < 7 then ignore @@ peek queue
+      if !len = n_messages - i + 1 then pop queue
+      else
+        let rand = Random.int 7 in
+        if rand < 2 then (
+          ignore @@ push queue i;
+          len := !len + 1)
+        else if rand < 4 then (
+          ignore @@ pop queue;
+          len := !len - 1)
+        else if rand < 5 then (
+          ignore @@ push_head queue i;
+          len := !len + 1)
+        else if rand < 6 then ignore @@ is_empty queue
+        else if rand < 7 then ignore @@ peek queue
     done
   in
   Printf.printf "%s  (one domain with all operations): %f per second\n%!" label
@@ -244,16 +262,18 @@ let () =
     (fun q -> Saturn.Queue.pop q |> ignore);
 
   print_endline "";
-
-  bench_single_length "Picos MPSC single    " Mpsc_queue_1.create
-    Mpsc_queue_1.push (fun q -> Mpsc_queue_1.pop q |> ignore);
-  bench_single_length "GADT implementation  "
-    Saturn.Single_consumer_queue.create Saturn.Single_consumer_queue.push
-    (fun q -> Saturn.Single_consumer_queue.pop q |> ignore);
-  bench_single_length "Saturn old MPSC queue " Mpsc_queue_old.create
-    Mpsc_queue_old.push (fun q -> Mpsc_queue_old.pop q |> ignore);
-  bench_single_length "Saturn queue          " Saturn.Queue.create
-    Saturn.Queue.push (fun q -> Saturn.Queue.pop q |> ignore);
+  [ 1; 10; 100; 1000; 5000 ]
+  |> List.iter (fun n ->
+         print_endline "";
+         bench_single_length n "Picos MPSC single    " Mpsc_queue_1.create
+           Mpsc_queue_1.push (fun q -> Mpsc_queue_1.pop q |> ignore);
+         bench_single_length n "GADT implementation  "
+           Saturn.Single_consumer_queue.create Saturn.Single_consumer_queue.push
+           (fun q -> Saturn.Single_consumer_queue.pop q |> ignore);
+         bench_single_length n "Saturn old MPSC queue " Mpsc_queue_old.create
+           Mpsc_queue_old.push (fun q -> Mpsc_queue_old.pop q |> ignore);
+         bench_single_length n "Saturn queue          " Saturn.Queue.create
+           Saturn.Queue.push (fun q -> Saturn.Queue.pop q |> ignore));
 
   print_endline "";
 
